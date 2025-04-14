71d04e07e26e0f0f1a25084390daf935d4e2a418
Maqsood Alam <malam84@gmail.com>
2025-04-14 16:12:52 +0800

    Update InfinispanRemoteConfig.java

--- src/main/java/com/rh/poc/common/InfinispanRemoteConfig.java
+++ src/main/java/com/rh/poc/common/InfinispanRemoteConfig.java
@@ -18,17 +18,27 @@ import com.rh.poc.db.UserSchemaInitializer;
 @EnableInfinispanRemoteHttpSession(cacheName = "sessions")
 public class InfinispanRemoteConfig {
 
-    @Value("${infinispan.remote.server-list}")
-	private String host;
-	
-	@Value("${infinispan.remote.ports}")
-	private int ssl_port;
-	
-	@Value("${infinispan.remote.auth-username}")
-	private String userName;
+    @Value("${infinispan.client.hotrod.server}")
+    private String host;
 
-	@Value("${infinispan.remote.auth-password}")
-	private String password;
+    @Value("${infinispan.client.hotrod.port}")
+    private int ssl_port;
+
+    @Value("${infinispan.client.hotrod.auth_username}")
+    private String userName;
+
+    @Value("${infinispan.client.hotrod.auth_password}")
+    private String password;
+	
+    @Value("${infinispan.client.hotrod.sasl_mechanism}")
+    private String saslMechanism;
+
+    @Value("${infinispan.client.hotrod.sni_host_name}")
+    private String sniHostName;
+
+    @Value("${infinispan.client.hotrod.trust_store_path}")
+    private String trustStorePath;
+
 
     @Bean
     public RemoteCacheManager remoteCacheManager() {
@@ -37,14 +47,19 @@ public class InfinispanRemoteConfig {
     	System.out.println(">>>>>>>>ssl_port>>>>>>>>>>" +ssl_port);
         ConfigurationBuilder builder = new ConfigurationBuilder();
         builder.addServer()
-               .host("epf-datagrid-poc.datagrid.svc.cluster.local") // Remote Infinispan server host
-               .port(11222)       // Remote Infinispan server port
+               .host(infinispan.client.hotrod.server) // Remote Infinispan server host
+               .port(ssl_port)       // Remote Infinispan server port
                .security()
                .authentication()
                .username(userName) // Username
                .password(password) // Password
                .saslMechanism("DIGEST-MD5")
+	       .ssl()
+	       .sniHostName(sniHostName)
+	       .trustStorePath(trustStorePath)
+	       .clientIntelligence(ClientIntelligence.BASIC);
                .addContextInitializer(new UserSchemaInitializer()); ; // SASL mechanism
+
         
         return new RemoteCacheManager(builder.build());
     }
