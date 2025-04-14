6e7ade95e8be2347d92166060202c473c2c5f898
Maqsood Alam <malam84@gmail.com>
2025-04-14 17:10:10 +0800

    Update InfinispanRemoteConfig.java

--- src/main/java/com/rh/poc/common/InfinispanRemoteConfig.java
+++ src/main/java/com/rh/poc/common/InfinispanRemoteConfig.java
@@ -19,27 +19,6 @@ import com.rh.poc.db.UserSchemaInitializer;
 @EnableInfinispanRemoteHttpSession(cacheName = "sessions")
 public class InfinispanRemoteConfig {
 
-  /*  @Value("${infinispan.client.hotrod.server}")
-    private String host;
-
-    @Value("${infinispan.client.hotrod.port}")
-    private int ssl_port;
-
-    @Value("${infinispan.client.hotrod.auth_username}")
-    private String userName;
-
-    @Value("${infinispan.client.hotrod.auth_password}")
-    private String password;
-	
-    @Value("${infinispan.client.hotrod.sasl_mechanism}")
-    private String saslMechanism;
-
-    @Value("${infinispan.client.hotrod.sni_host_name}")
-    private String sniHostName;
-
-    @Value("${infinispan.client.hotrod.trust_store_path}")
-    private String trustStorePath; */
-
     @Bean
     public RemoteCacheManager remoteCacheManager() {
         return new RemoteCacheManager(
