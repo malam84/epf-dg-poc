f6de5937e23654691385e6b3dfbf045f0edf840f
Maqsood Alam <malam84@gmail.com>
2025-04-16 15:04:03 +0800

    Update SessionController.java

--- src/main/java/com/rh/poc/session/SessionController.java
+++ src/main/java/com/rh/poc/session/SessionController.java
@@ -9,16 +9,20 @@ public class SessionController {
 
     @PostMapping("/set")
     public String setSession(@RequestParam String key, @RequestParam String value, HttpSession session) {
-    	System.out.println("hello "+session);
+        System.out.println(" set key >>>>>>>>>>>>>>>>>>>>>> "+key);
+    	System.out.println("set value >>>>>>>>>>>>>>>>>>>>>> "+value);
+        System.out.println("hello "+session);
         session.setAttribute(key, value);
+        System.out.println(">>>>>>>>>>>>>>end set >>>>>>>>>>>>>>>>>>>>>>");
         return "Session attribute set: " + key + " = " + value;
     }
 
     @GetMapping("/get")
     public String getSession(@RequestParam String key, HttpSession session) {
-    	
+    	System.out.println("get key >>>>>>>>>>>>>>>>>>>>>> "+key);
     	System.out.println(session.getAttributeNames().nextElement());
         Object value = session.getAttribute(key);
+        System.out.println(">>>>>>>>>>end get key >>>>>>>>>>>>>>>>>>>>>> "+key);
         return value != null ? "Session attribute: " + key + " = " + value : "No value found";
     }
     
